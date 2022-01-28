4001d05b0ac737b1a96a3935171526d7eb152199
danvdm <d.v.d.meer473@gmail.com>
2022-01-28 20:14:23 +0100

    pls work

--- R/RandomArt.R
+++ R/RandomArt.R
@@ -1,18 +1,35 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+# Random art
+# Daniel van der Meer
 
-hello <- function() {
-  print("Hello, world!")
+make_art <- function(seed){
+  set.seed(seed)
+  x <- seq(-20, 20, 0.1)
+  spread = sample(seq(-0.2, 0.4), 1)
+  slope = sample(seq(-10, 0, 1), 1)
+
+  range_spread <- seq(spread, spread+4, 0.2)
+  range_slope <- seq(slope, slope+20, 0.3)
+
+  l <- slope*x+3
+  par(bg = colors()[sample(c(2:600), 1)])
+  plot(NULL, xlim = c(-20, -10),
+       ylim = c(-20, -10),
+       frame = F,
+       axes = F,
+       xlab = "",
+       ylab = "")
+  col <- 1
+  for (i in range_spread){
+    for (j in range_slope){
+      lines(x, i*sin(i*x)+(j*x+3),
+            col = col,
+            lw = 4)
+      col = col+1
+      if (col == 657){
+        col = 1
+      }
+    }
+  }
 }
+
+
